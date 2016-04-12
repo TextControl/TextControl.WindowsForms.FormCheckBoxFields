789f06ba6c781d2da85b0e133c79e86998759f67
Gunnar <gunnar@textcontrol.com>
2016-04-12 11:30:54 +0200

    Added content

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace tx_checkbox_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textControl1 = new TXTextControl.TextControl();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 20F);
+            this.textControl1.Location = new System.Drawing.Point(25, 102);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.PageMargins.Bottom = 79.03D;
+            this.textControl1.PageMargins.Left = 79.03D;
+            this.textControl1.PageMargins.Right = 79.03D;
+            this.textControl1.PageMargins.Top = 79.03D;
+            this.textControl1.Size = new System.Drawing.Size(916, 373);
+            this.textControl1.TabIndex = 0;
+            this.textControl1.TextFieldClicked += new TXTextControl.TextFieldEventHandler(this.textControl1_TextFieldClicked);
+            this.textControl1.TextFieldCreated += new TXTextControl.TextFieldEventHandler(this.textControl1_TextFieldCreated);
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 475);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(941, 22);
+            this.statusBar1.TabIndex = 1;
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBar1.Location = new System.Drawing.Point(0, 49);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(941, 28);
+            this.buttonBar1.TabIndex = 2;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 102);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 373);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rulerBar2.Location = new System.Drawing.Point(0, 77);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(941, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(941, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(111, 22);
+            this.toolStripButton1.Text = "Insert CheckBox";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(941, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.printToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.loadToolStripMenuItem.Text = "Load...";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.saveToolStripMenuItem.Text = "Save...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(106, 6);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 497);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TX Text Control Sample: Inserting and Processing CheckBoxes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private TXTextControl.RulerBar rulerBar2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TXTextControl;
+using TXTextControl.DocumentServer.Fields;
+
+namespace tx_checkbox_sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textControl1.RulerBar = rulerBar2;
+            textControl1.ButtonBar = buttonBar1;
+            textControl1.StatusBar = statusBar1;
+            textControl1.VerticalRulerBar = rulerBar1;
+        }
+
+        private string UNCHECKED = UnicodeHexToString("\\u2610");
+        private string CHECKED = UnicodeHexToString("\\u2612");
+
+        // u2611: Use this value to use a checkmark instead the default 'X'
+        //private string CHECKED = UnicodeHexToString("\\u2611");
+
+        private static string UnicodeHexToString(string text)
+        {
+            // returns the string representation
+            return System.Text.Encoding.Unicode.GetString(BitConverter.GetBytes(short.Parse(text.Substring(2), System.Globalization.NumberStyles.HexNumber)));
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            InsertCheckBox();
+        }
+
+        private void InsertCheckBox()
+        {
+            // we use an Unicode font to simulate the check box
+            textControl1.Selection.FontName = "Arial Unicode MS";
+
+            // create a new checkbox using the specific adapter field class
+            FormCheckBox clsCheckBox = new TXTextControl.DocumentServer.Fields.FormCheckBox();
+            clsCheckBox.Text = UNCHECKED;
+            clsCheckBox.Enabled = true;
+            clsCheckBox.ApplicationField.Editable = false;
+            clsCheckBox.ApplicationField.DoubledInputPosition = true;
+            
+            // add the ApplicationField to the document
+            textControl1.ApplicationFields.Add(clsCheckBox.ApplicationField);
+        }
+
+        public void SetFields()
+        {
+            // loop through all fields to activate the checkbox fields
+            foreach (TXTextControl.ApplicationField appfield in textControl1.ApplicationFields)
+            {
+                if ((appfield.TypeName != "FORMCHECKBOX"))
+                    return;
+
+                // create a new adapter field
+                FormCheckBox ChkBoxField = new FormCheckBox(appfield);
+
+                // select the field to change the font name
+                textControl1.Select(ChkBoxField.Start - 1, ChkBoxField.Length);
+                textControl1.Selection.FontName = "Arial Unicode MS";
+
+                // set the text (state)
+                if (ChkBoxField.Checked == true)
+                    ChkBoxField.Text = CHECKED;
+                else
+                    ChkBoxField.Text = UNCHECKED;
+
+                textControl1.Select(0, 0);
+            }
+        }
+
+        private void textControl1_TextFieldClicked(object sender, TXTextControl.TextFieldEventArgs e)
+        {
+            // cast the field to an ApplicationField
+            ApplicationField field = e.TextField as ApplicationField;
+
+            if (field != null)
+            {
+                // check whether the field is a checkbox field
+                if ((field.TypeName == "FORMCHECKBOX"))
+                {
+                    // create a new adapter field
+                    FormCheckBox chkb = new FormCheckBox(field);
+
+                    if (chkb.Enabled == false)
+                        return;
+
+                    // change the checked state
+                    if (field.Text == UNCHECKED)
+                    {
+                        chkb.Checked = true;
+                        chkb.Text = CHECKED;
+                    }
+                    else if (field.Text == CHECKED)
+                    {
+                        chkb.Checked = false;
+                        chkb.Text = UNCHECKED;
+                    }
+                }
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // load the document
+            // LoadSettings must be adjusted to load the MS Word fields
+            LoadSettings ls = new LoadSettings();
+            ls.ApplicationFieldFormat = ApplicationFieldFormat.MSWord;
+            textControl1.Load(StreamType.RichTextFormat | StreamType.MSWord | StreamType.WordprocessingML, ls);
+
+            SetFields();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // save the document
+            SaveSettings ss = new SaveSettings();
+            textControl1.Save(StreamType.RichTextFormat | StreamType.MSWord | StreamType.WordprocessingML, ss);
+        }
+
+        private void textControl1_TextFieldCreated(object sender, TextFieldEventArgs e)
+        {
+            int iStartPos = textControl1.Selection.Start;
+            SetFields();
+            textControl1.Selection.Start = iStartPos;
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textControl1.Print("TX Text Control Document");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textControl1.Tables.GridLines = false;
+        }
+    }
+}
